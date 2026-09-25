447e1daeacbf6bdb70c5a643d9a1df8601e54157
cherrypick6641 <lnowosie@gmail.com>
2026-09-25 12:18:37 +0200

    bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId  = "com.openlauncher.app"
         minSdk         = 21
         targetSdk      = 36
-        versionCode    = 20
-        versionName    = "2.0.1"
+        versionCode    = 21
+        versionName    = "2.0.2"
     }
 
     signingConfigs {
